dca3cc77072c9b5c547ccba17dada6de0086f90a
Erich Parmejano <erich.parmejano@nubank.com.br>
2022-06-03 18:25:02 -0300

    finished mapsflatmapsfor

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -197,4 +197,12 @@ object ListTest extends App {
   println(anotherListOfIntegers.zipWith[String, String](listOfStrings, _ + "-" + _))
 
   println(listOfIntegers.fold(0)(_ + _))
+
+  // for comprehensions
+  val combinations = for {
+    n      <- listOfIntegers
+    string <- listOfStrings
+  } yield n + "-" + string
+  println(combinations)
+
 }
